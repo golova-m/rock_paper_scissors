75afcc66d8c8be0aaa8d4f180a9fa963b125aca0
Marina <golova.m@bk.ru>
2024-04-23 00:26:43 +0400

    Add files via upload
    First version mini game rock_paper_scissors

---
+++ main.py
@@ -0,0 +1,59 @@
+from tkinter import *
+import random as rdm
+
+class Main(Frame):
+    def __init__(self, window):
+        super(Main, self).__init__(window)
+        self.startUI()
+
+    def start_btn(self):
+        btn_rock = Button(window, text='Камень', command=lambda x=1:self.btn_click(x))
+        btn_scissors = Button(window, text='Ножницы', command=lambda x=1:self.btn_click(x))
+        btn_paper = Button(window, text='Бумага', command=lambda x=1:self.btn_click(x))
+
+        btn_rock.place(x=10, y=250, width=100, height=50)
+        btn_scissors.place(x=150, y=250, width=100, height=50)
+        btn_paper.place(x=290, y=250, width=100, height=50)
+
+    def start_lbl(self):
+        self.lbl_start = Label(window, text='Начинаем!')
+        self.lbl_score = Label(window, justify="left", text=f"Побед: {self.win}\nПроигрышей:{self.lose}\nДружба: {self.drow}")
+
+        self.lbl_start.place(x=100, y=150)
+        self.lbl_score.place(x=165, y=50)
+
+    def btn_click(self, play_choise):
+        comp_choise = rdm.randint(1, 3)
+        if play_choise == comp_choise:
+            self.drow += 1
+            self.lbl_score.configure(text="Ничья")
+        elif play_choise == 1 and comp_choise == 2 \
+            or play_choise == 2 and comp_choise == 3 \
+            or play_choise == 3 and comp_choise == 1:
+            self.win += 1
+            self.lbl_score.configure(text="Победа")
+        else:
+            self.lose += 1
+            self.lbl_score.configure(text="Проигрыш")
+
+        self.lbl_score.configure(text=f"Побед: {self.win}\nПроигрышей:{self.lose}\Дружба: {self.drow}")
+
+        del comp_choise
+
+    def startUI(self):
+        self.win = self.drow = self.lose = 0
+
+        self.start_btn()
+        self.start_lbl()
+        
+
+if __name__ == '__main__':
+    window = Tk()
+    window.title('Rock Scissors Paper')
+    window.geometry('400x400+300+300')
+    window.resizable(False, False)
+    app = Main(window)
+    app.pack()
+    window.mainloop()
+    
+    
